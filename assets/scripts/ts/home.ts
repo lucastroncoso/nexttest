577cf555b67db9b0620a729fe96f4dd5ff36e1d9
Sebastian Curutchet Peluso <sebastiancurutchetpeluso@MBPdeSebastian.home>
2020-09-17 04:59:19 -0300

    fix

--- assets/scripts/ts/home.ts
+++ assets/scripts/ts/home.ts
@@ -11,36 +11,42 @@ export class Home {
 
         window.onscroll = function(event) {
 
-            if(registerStepActive){
+            
 
                 let registerStepsElement = (<HTMLElement>document.querySelector("#registerSteps"));
                 let diff = registerStepsElement.offsetTop - (window.pageYOffset || document.documentElement.scrollTop) - window.innerHeight;
                 let percent = Math.abs(diff) / registerStepsElement.clientHeight * 100;
-                
+                let value = {
+                    1: 0,
+                    2: 20,
+                    3: 50,
+                    4: 80
+                };
+
 
                 if(diff < 0){
 
+
+            if(registerStepActive){
+
                     
-                    if(percent < 25){
-                        if(registerStep < 1) registerStep = 1;
-                    }else if(percent > 40 && percent < 60){
-                        if(registerStep < 2) registerStep = 2;
-                    }else if(percent > 60 && percent < 75 ){
-                        if(registerStep < 3) registerStep = 3;
-                    }else if(percent > 75 && percent < 90 ){
-                        if(registerStep < 4) registerStep = 4;
-                    }else if(percent > 90){
-                        if(registerStep == 4) registerStepActive = false;
-                    }
 
-                        let value = {
-                            1: 0,
-                            2: 20,
-                            3: 50,
-                            4: 80
-                        };
+                if(percent < 25){
+                    if(registerStep < 1) registerStep = 1;
+                }else if(percent > 40 && percent < 60){
+                    if(registerStep < 2) registerStep = 2;
+                }else if(percent > 60 && percent < 75 ){
+                    if(registerStep < 3) registerStep = 3;
+                }else if(percent > 75 && percent < 90 ){
+                    if(registerStep < 4) registerStep = 4;
+                }else if(percent > 90){
+                    if(registerStep == 4) registerStepActive = false;
+                }
 
-                        document.documentElement.style.setProperty('--register-step', `${value[registerStep]}%`);
+                document.documentElement.style.setProperty('--register-step', `${value[registerStep]}%`);
+                document.querySelector(`#registerStep${registerStep}`).classList.add("opacityDisable")
+                     
+
 
                 }
             }
