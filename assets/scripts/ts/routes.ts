415a860b756f6c36ce4178a3b76ff61b63811c12
lucastroncoso <lucastroncoso.seguros@gmail.com>
2021-06-24 22:39:20 -0300

    legales

--- assets/scripts/ts/routes.ts
+++ assets/scripts/ts/routes.ts
@@ -17,8 +17,8 @@ export class Routes {
         "/sumate": { page: "carrers-list", header: false, footer: false },
         "/seguridad": { page: "security", header: true, footer: true },
         "/faqs": { page: "faq", header: true, footer: true },
-        "/legales" : { page: "privacy", header: true, footer: true },
-        "/transferencias": { page: "transfers", header: true, footer: true }, 
+        //"/privacidad" : { page: "privacy", header: true, footer: true },
+        "/transferencias": { page: "transfers", header: true, footer: true },
         //"/pagos" : { page: "payments", header: true, footer: true },
         "/Consulta-los-costos-y-las-comisiones-de-nuestros-productos": { page: "charge", header: true, footer: true },
         "/reseteo-de-pin": { page: "landing-reset-pin", header: false, footer: false },
@@ -69,7 +69,7 @@ export class Routes {
         "/giveawayInstagram": { page: "", header: false, footer: false, redirect: "https://statics.mx.ua.la/tyc/Dina%CC%81mica_Giveaway_Instagram_Uala%CC%81_Me%CC%81xico%20_2021.pdf" },
         "/giveawayTwitter": { page: "", header: false, footer: false, redirect: "https://statics.mx.ua.la/tyc/Dina%CC%81mica_Giveaway_Twitter_Uala%CC%81_Me%CC%81xico_2021.pdf" },
         "/legales50pesosAbril": { page: "", header: false, footer: false, redirect: "/legal/legales-promocion-50-pesos-Abril.pdf" },
-        "/legales50pesosMayo": { page: "", header: false, footer: false, redirect: "legal/legales-50pesos-mayo.pdf" },
+        "/legales50pesosMayo": { page: "", header: false, footer: false, redirect: "/legal/Legales-Promo-50-pesos-Junio.pdf" },
         "/tyc_programa_referidos": { page: "tyc_programa_referidos", header: false, footer: false }
     };
 
