067c38492bd912e538f4c2ba6ada86457f120e83
sebastian <sebastiancuruchet@gmail.com>
2021-07-26 12:48:01 -0300

    safelist tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,13 @@
 module.exports = {
-  purge: ['./src/pages/**/*.{js,ts,jsx,tsx}','./src/components/**/*.{js,ts,jsx,tsx}'],
+  purge: {
+    content: ['./src/pages/**/*.{js,ts,jsx,tsx}','./src/components/**/*.{js,ts,jsx,tsx}'],
+    safelist: [
+      'w-1/4',
+      'w-2/4',
+      'w-3/4',
+      'w-4/4'
+    ]
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
